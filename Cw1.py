02674160b47ba61b434eb00ed0831ad80b367820
Bartosz Kowalewski <209365@edu.p.lodz.pl>
2019-03-26 23:30:23 +0100

    [Start] Very hard beginnings with Python in AI

--- Cw1.py
+++ Cw1.py
@@ -2,6 +2,7 @@ from random import randint
 from math import sqrt
 #miasta = {"Nr": 0, "Ox": 0, "Oy": 0}
 miasta = {}
+way_to_now = {}
 
 for m in range (4):
     # miasta["Nr"] = m
@@ -14,21 +15,32 @@ for m in range (4):
 
 #długość drogi miedzy miastami
 def distance(miasta, nrA, nrB):
-    d = sqrt((miasta[nrA][0]-miasta[nrB][0])**2 + (miasta[nrA][1]-miasta[nrB][1])**2)
-    return d
+    if miasta[nrA][0] is None and miasta[nrB][0] is None and miasta[nrA][1] is None and miasta[nrB][1] is None:
+        d = sqrt((miasta[nrA][0]-miasta[nrB][0])**2 + (miasta[nrA][1]-miasta[nrB][1])**2)
+        return d
 
-distance(miasta,0,1)
-
-def take_best_way(miasta):
+def take_best_way(miasta, nr_now, maxi):
     d = {}
-    for i in range(1,3):
-        d[i] = distance(miasta,0,i)
-    w=0
-    for m in range(3):
+    for i in range(1,maxi+1):
+        d[i-1] = distance(miasta,nr_now,i)
+    city_direction = {}
+    city_direction[0]=0
+    city_direction[1]=1
+    print 
+    for m in range(maxi):
         if m==0 :
-            w=d[m]
-        elif w>d[m]:
-            w=d[m]
-    print(w)
+            city_direction[0] = d[m]
+        elif city_direction[0]>d[m]:
+            city_direction[0]=d[m]
+            city_direction[1] += m 
+    return city_direction
 
-take_best_way(miasta)
+nr_now = 0
+maxi = 3
+for i in range(3):
+    way_to_now[i] = take_best_way(miasta,nr_now,maxi)
+    nr_now = way_to_now[i][1]
+    del miasta[way_to_now[i][1]]
+    maxi -= 1
+
+print(way_to_now)
